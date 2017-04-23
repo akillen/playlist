8b242e738ecc87e5832866bb37c9420a2a8a371d
Aaron Killen <akillen@gmail.com>
2017-04-23 15:43:28 -0400

    generate m3u using id3 track metadata for files in a directory

--- mp3.js
+++ mp3.js
@@ -4,20 +4,43 @@ const fs = require('fs');
 const workingDir = (process.argv[2] || __dirname);
 
 var dir = fs.readdirSync(workingDir);
-console.log(dir);
 
+console.log(dir);
 var files = [];
 dir.forEach((file, inx, arr) => {
-  fs.readFile(path.join(workingDir,file), (err, buff)=>{
+  fs.readFile(path.join(workingDir, file), (err, mp3) => {
+    if (err)
+      console.log(err);
     //push to array
-    id3.parse(mp3).then(tag => { 
-      console.log(`pushed ${tag.title}`);
-      files.push(tag);
+    id3.parse(mp3).then(tag => {
+      if (typeof tag != 'undefined' && tag.version) {
+        tag.filename = file;
+        files.push(tag);
+      }
+      if (files.length == dir.length - 1) {
+        files.sort((a, b) => {
+          let atrack = Number(a.track.split('/')[0]);
+          let btrack = Number(b.track.split('/')[0]);
+
+          if (atrack < btrack)
+            return -1;
+          if (atrack == btrack)
+            return 0;
+          if (atrack > btrack)
+            return 1;
+        });
+        console.log(files);
+        makeM3u(files.map(file => {
+          return file.filename;
+        }));
+      }
+    }).catch(err => {
+      console.log(err);
     });
   });
 });
 
-while(files.length < dir.length-1){
-
+function makeM3u(files) {
+  console.log(files);
+  fs.writeFile(workingDir.substr(0, workingDir.length - 1) + '.m3u', files.join('\n'), err => { console.log(err); });
 }
-console.log(files);
